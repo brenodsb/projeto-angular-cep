c176794a65e93b1ec959f407a3b99529289e1f0c
Breno de Sousa Braga <brenosousabraga@gmail.com>
2024-05-02 13:20:51 -0300

    fix: altera expect de titulo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'angular-cep' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-cep');
+    expect(app['title']).toEqual('angular-cep');
   });
 
   it('should render title', () => {
